69b762f938e8bd3281c20a63bff0c683bf2187be
z0es0 <157841615+z0es0@users.noreply.github.com>
2024-10-04 09:58:44 -0400

    working on adding elements to page

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -2,6 +2,22 @@
 
 let cart = [];
 
+// create glazing and pack size option object
+let glazingOptions = {
+    'Keep original': 0,
+    'Sugar milk': 0,
+    'Vanilla milk': .5,
+    'Double chocolate': 1.5
+};
+  
+let packSizeOptions = {
+    '1': 1,
+    '3': 3,
+    '6': 5,
+    '12': 10
+};
+
+// create Roll class
 class Roll {
     constructor(rollType, rollGlazing, packSize, rollPrice) {
         this.type = rollType;
@@ -9,69 +25,69 @@ class Roll {
         this.size = packSize;
         this.basePrice = rollPrice;
 
+        this.totalPrice = this.calculatePrice(rollType, rollGlazing, packSize, rollPrice);
+
         this.element = null;
+    }
+
+    calculatePrice(rollType, rollGlazing, packSize, rollPrice) {
+        let glazePriceAddition = glazingOptions[rollGlazing];
+        // console.log(glazePriceAddition);
+
+        let packSizePriceMultiplier = packSizeOptions[packSize];
+        // console.log(packSizePriceMultiplier);
+
+        let totalPrice = (rollPrice + glazePriceAddition) * packSizePriceMultiplier;
+        // console.log(totalPrice); 
+        return totalPrice
     }
 }
 
+// add items to cart array
 function addCartItem(rollType, rollGlazing, packSize, rollPrice) {
-    const cartItem = new Roll(rollType, rollGlazing, packSize, rollPrice);
+    let cartItem = new Roll(rollType, rollGlazing, packSize, rollPrice);
     cart.push(cartItem);
+    console.log(cart);
     return cart;
 }
 
-// add rolls to cart 
+// create roll objects in cart
 const rollOne = addCartItem('Original', 'Sugar milk', '1', 2.49);
 const rollTwo = addCartItem('Walnut', 'Vanilla milk', '12', 3.49);
 const rollThree = addCartItem('Raisin', 'Sugar milk', '3', 2.99);
-const rollFour = addCartItem('Apple', 'Original', '3', 3.49);
+const rollFour = addCartItem('Apple', 'Keep original', '3', 3.49);
 
+console.log(cart)
 
+// put all roll objects on page
+for (const roll of cart) {
+    createElement(roll);
+  }
 
-// from hw4
+// add to page
+function createElement(roll) { 
+    const template = document.querySelector('#cart-item-template');
+    const clone = template.content.cloneNode(true);
+    roll.element = clone.querySelector('.cart-item');
 
-// function to calculate and display total price (moved from above)
-let basePrice = rolls[rollFlavor].basePrice; // updated to be based on param
+    // const btnDelete = roll.element.querySelector('.remove-button');
+    // btnDelete.addEventListener('click', () => {
+    //     deleteRoll(roll);
+    // };
 
-function displayPrice() {
-    let glazePriceAddition = parseFloat(selectGlazingElement.value);
-    let packSizePriceMultiplier = parseFloat(selectPackSizeElement.value);
-
-    let totalPrice = (basePrice + glazePriceAddition) * packSizePriceMultiplier; 
-
-    let totalPriceElement = document.querySelector('#total-price');
-    totalPriceElement.innerText = "$ " + totalPrice.toFixed(2);
+    const rollListElement = document.querySelector('#cart-list');
+    rollListElement.prepend(roll.element);
+    updateElement(roll)
 }
 
-// display price on load
-displayPrice();
+function updateElement(roll) {
+    const rollImageElement = roll.element.querySelector('.roll-thumbnail');
+    const rollTitleElement = roll.element.querySelector('.roll-title');
+    const rollGlazeElement = roll.element.querySelector('.note-body');
 
-// put all notecards on page
-// for (const notecard of notecardSet) {
-//     createElement(notecard);
-//   }
-
-// // add to page
-// function createElement(notecard) { 
-//     const template = document.querySelector('#notecard-template');
-//     const clone = template.content.cloneNode(true);
-//     notecard.element = clone.querySelector('.notecard');
-
-//     const btnDelete = notecard.element.querySelector('.icon-delete');
-//     btnDelete.addEventListener('click', () => {
-//         deleteNote(notecard);
-//     });
-
-//     const notecardListElement = document.querySelector('#notecard-list');
-//     notecardListElement.prepend(notecard.element);
-//     updateElement(notecard)
-// }
-
-// function updateElement(notecard) {
-//     const noteImageElement = notecard.element.querySelector('.notecard-thumbnail');
-//     const noteTitleElement = notecard.element.querySelector('.note-title');
-//     const noteBodyElement = notecard.element.querySelector('.note-body');
-
-//     noteImageElement.src = notecard.noteImageURL;
-//     noteTitleElement.innerText = notecard.noteTitle;
-//     noteBodyElement.innerText = notecard.noteBody;
-// }
+    let imageSrc = rolls[roll.type].imageFile;
+    rollImageElement.src = imageSrc;
+    
+    rollTitleElement.innerText = roll.type + ' Cinnamon Roll';
+    noteBodyElement.innerText = notecard.noteBody;
+}
