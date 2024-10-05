6dc8b57d04092904d7d578c7222ec87206007741
z0es0 <157841615+z0es0@users.noreply.github.com>
2024-10-05 12:49:40 -0400

    finished hw5 and changed array to set

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -1,6 +1,7 @@
 // ---------------------- HW 5: ADD & DELETE FROM CART ON CART PAGE ------
+// REFERENCES: https://www.w3schools.com/js/js_if_else.asp
 
-let cart = [];
+let cart = new Set();
 
 // create glazing and pack size option object
 let glazingOptions = {
@@ -32,13 +33,11 @@ class Roll {
 
     calculatePrice(rollType, rollGlazing, packSize, rollPrice) {
         let glazePriceAddition = glazingOptions[rollGlazing];
-        // console.log(glazePriceAddition);
 
         let packSizePriceMultiplier = packSizeOptions[packSize];
-        // console.log(packSizePriceMultiplier);
 
-        let totalPrice = (rollPrice + glazePriceAddition) * packSizePriceMultiplier;
-        // console.log(totalPrice); 
+        let totalPrice = ((rollPrice + glazePriceAddition) * packSizePriceMultiplier).toFixed(2);
+
         return totalPrice
     }
 }
@@ -46,18 +45,15 @@ class Roll {
 // add items to cart array
 function addCartItem(rollType, rollGlazing, packSize, rollPrice) {
     let cartItem = new Roll(rollType, rollGlazing, packSize, rollPrice);
-    cart.push(cartItem);
-    console.log(cart);
+    cart.add(cartItem);
     return cart;
 }
 
 // create roll objects in cart
-const rollOne = addCartItem('Original', 'Sugar milk', '1', 2.49);
-const rollTwo = addCartItem('Walnut', 'Vanilla milk', '12', 3.49);
-const rollThree = addCartItem('Raisin', 'Sugar milk', '3', 2.99);
 const rollFour = addCartItem('Apple', 'Keep original', '3', 3.49);
-
-console.log(cart)
+const rollThree = addCartItem('Raisin', 'Sugar milk', '3', 2.99);
+const rollTwo = addCartItem('Walnut', 'Vanilla milk', '12', 3.49);
+const rollOne = addCartItem('Original', 'Sugar milk', '1', 2.49);
 
 // put all roll objects on page
 for (const roll of cart) {
@@ -70,24 +66,58 @@ function createElement(roll) {
     const clone = template.content.cloneNode(true);
     roll.element = clone.querySelector('.cart-item');
 
-    // const btnDelete = roll.element.querySelector('.remove-button');
-    // btnDelete.addEventListener('click', () => {
-    //     deleteRoll(roll);
-    // };
+    const btnDelete = roll.element.querySelector('.remove-button');
+    btnDelete.addEventListener('click', () => {
+        deleteRoll(roll);
+    });
 
     const rollListElement = document.querySelector('#cart-list');
     rollListElement.prepend(roll.element);
-    updateElement(roll)
+    updateElement(roll);
+
+    calculateCartTotal();
 }
 
 function updateElement(roll) {
+    // get elements
     const rollImageElement = roll.element.querySelector('.roll-thumbnail');
     const rollTitleElement = roll.element.querySelector('.roll-title');
-    const rollGlazeElement = roll.element.querySelector('.note-body');
+    const rollGlazeElement = roll.element.querySelector('.roll-glaze');
+    const rollPackSizeElement = roll.element.querySelector('.roll-pack-size');
+    const rollPriceElement = roll.element.querySelector('.price');
+    const totalPriceElement = document.querySelector('.total-price');
 
-    let imageSrc = rolls[roll.type].imageFile;
+    // image
+    let imageSrc = './assets/products/' + rolls[roll.type].imageFile;
     rollImageElement.src = imageSrc;
     
+    // glazing
+    if (roll.glazing === 'Keep original') {
+        rollGlaze = 'Original';
+    } else {
+        rollGlaze = roll.glazing;
+    }
+    rollGlazeElement.innerText = 'Glazing: ' + rollGlaze;
+
     rollTitleElement.innerText = roll.type + ' Cinnamon Roll';
-    noteBodyElement.innerText = notecard.noteBody;
+    rollPackSizeElement.innerText = 'Pack size: ' + roll.size;
+    rollPriceElement.innerText = '$ ' + roll.totalPrice;
+}
+
+// delete roll
+function deleteRoll(roll) {
+    roll.element.remove();
+    cart.delete(roll);
+    calculateCartTotal();
+}
+
+// calculate total price
+function calculateCartTotal() {
+    let totalPrice = 0;
+    for (const roll of cart) {
+        totalPrice += parseFloat(roll.totalPrice);
+    }
+
+    const cartTotalElement = document.querySelector('.total-price');
+    cartTotalElement.innerText = '$ ' + totalPrice.toFixed(2);
 }
